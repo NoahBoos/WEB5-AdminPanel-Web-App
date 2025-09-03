c0bd7f3b87e17f812fe9e47cee48893dd722e91b
Noah Boos <nboosgamemaker@gmail.com>
2025-09-03 17:53:20 +0200

    Fonc. - Fragmentation PHP ; Ajout de la page par défaut.

--- public/index.php
+++ public/index.php
@@ -88,88 +88,13 @@
         </aside>
         <div class="flex flex-col flex-1 overflow-y-auto">
             <main class="flex-1">
-                <div class="flex flex-col justify-center items-center p-6 gap-6 lg:flex-row lg:flex-wrap flex-1 lg:items-start">
-                    <section class="flex flex-col justify-center items-center gap-6 lg:flex-row lg:flex-wrap lg:w-[48%] lg:items-start">
-                        <article class="flex flex-col w-full border border-gray-200 rounded-md">
-                            <div class="bg-gray-100 sm:text-lg overflow-hidden flex flex-row justify-between items-center px-3 py-2">
-                                <h1>
-                                    Pages récentes
-                                </h1>
-                                <button class="bg-secondary-400 text-white flex flex-row justify-center items-center w-fit p-2 gap-2 border-0 rounded-md">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-                                    Créer une page
-                                </button>
-                            </div>
-                            <div class="text-sm sm:text-base flex flex-col justify-center items-start p-3 gap-6">
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia fermentum justo, at semper est. Phasellus at lacinia nunc.
-                                </p>
-                                <a class="text-secondary-400 uppercase font-semibold">
-                                    Voir toutes les pages
-                                </a>
-                            </div>
-                        </article>
-                        <article class="flex flex-col w-full border border-gray-200 rounded-md">
-                        <div class="bg-gray-100 sm:text-lg overflow-hidden flex flex-row justify-between items-center px-3 py-2">
-                            <h1>
-                                Articles récents
-                            </h1>
-                            <button class="bg-secondary-400 text-white flex flex-row justify-center items-center w-fit p-2 gap-2 border-0 rounded-md">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-                                Créer un article
-                            </button>
-                        </div>
-                        <div class="text-sm sm:text-base flex flex-col justify-center items-start p-3 gap-6">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia fermentum justo, at semper est. Proin non aliquam ipsum. Vestibulum tincidunt vitae orci non eleifend. Suspendisse hendrerit est a est pharetra mollis. Phasellus at lacinia nunc.
-                            </p>
-                            <a class="text-secondary-400 uppercase font-semibold">
-                                Voir toutes les pages
-                            </a>
-                        </div>
-                    </article>
-                    </section>
-                    <section class="flex flex-col justify-center items-center gap-6 lg:flex-row lg:flex-wrap lg:w-[48%] lg:items-start">
-                        <article class="flex flex-col w-full border border-gray-200 rounded-md">
-                            <div class="bg-gray-100 sm:text-lg overflow-hidden flex flex-row justify-between items-center px-3 py-2">
-                                <h1>
-                                    Inscriptions récentes
-                                </h1>
-                                <button class="bg-secondary-400 text-white flex flex-row justify-center items-center w-fit p-2 gap-2 border-0 rounded-md">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-                                    Créer un utilisateur
-                                </button>
-                            </div>
-                            <div class="text-sm sm:text-base flex flex-col justify-center items-start p-3 gap-6">
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia fermentum justo, at semper est. Proin non aliquam ipsum. Vestibulum tincidunt vitae orci non eleifend. Suspendisse hendrerit est a est pharetra mollis. Phasellus at lacinia nunc.
-                                </p>
-                                <a class="text-secondary-400 uppercase font-semibold">
-                                    Voir toutes les pages
-                                </a>
-                            </div>
-                        </article>
-                        <article class="flex flex-col w-full border border-gray-200 rounded-md">
-                            <div class="bg-gray-100 sm:text-lg overflow-hidden flex flex-row justify-between items-center px-3 py-2">
-                                <h1>
-                                    Statistiques
-                                </h1>
-                                <button class="bg-secondary-400 text-white flex flex-row justify-center items-center w-fit p-2 gap-2 border-0 rounded-md">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-                                    Créer une page
-                                </button>
-                            </div>
-                            <div class="text-sm sm:text-base flex flex-col justify-center items-start p-3 gap-6">
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia fermentum justo, at semper est. Proin non aliquam ipsum. Vestibulum tincidunt vitae orci non eleifend. Suspendisse hendrerit est a est pharetra mollis. Phasellus at lacinia nunc.
-                                </p>
-                                <a class="text-secondary-400 uppercase font-semibold">
-                                    Voir toutes les pages
-                                </a>
-                            </div>
-                        </article>
-                    </section>
-                </div>
+                <?php
+                    $currentURL = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+                    if (str_ends_with($currentURL, "/")) {
+                        include('./pages/dashboard.php');
+                    }
+                ?>
             </main>
             <footer class="p-6 bg-slate-700 text-white">
                 <p class="text-center">
